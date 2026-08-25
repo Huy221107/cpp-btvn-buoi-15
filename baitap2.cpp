5ee65d573aabf20b2ade0f5de4cac484cf793ded
Huy221107 <106250073@sv1.dut.udn.vn>
2026-08-25 22:26:18 +0700

    Keep only baitap1 and baitap2

--- baitap2.cpp
+++ baitap2.cpp
@@ -6,21 +6,22 @@ int main()
     cout << "nhap vao so nguyen duong :";
     cin >> n;
     int a[n];
-    cout<< "nhap vao cac phan tu cua mang :"<<endl;
+    cout << "nhap vao cac phan tu cua mang :" << endl;
     for (int i = 0; i < n; i++)
     {
         cout << " ";
         cin >> a[i];
     }
-    cout<< "mang goc :"<<endl;
-     for (int i = 0; i < n; i++)
+    cout << "mang goc :" << endl;
+    for (int i = 0; i < n; i++)
     {
-        cout<< a[i]<<" ";
+        cout << a[i] << " ";
     }
-    cout<<endl;
-    cout<<"mang sau khi dao nguoc thu tu :"<<endl;
-    for(int i=n-1;i>-1;i--){
-       cout<< a[i]<<" ";
+    cout << endl;
+    cout << "mang sau khi dao nguoc thu tu :" << endl;
+    for (int i = n - 1; i > -1; i--)
+    {
+        cout << a[i] << " ";
     }
     return 0;
 }
